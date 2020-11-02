8f8afc6ea839d0f7dcd09a2f9ff5453c97404359
Umair Yasin <umair_yasin@hotmail.com>
2020-11-03 04:42:52 +0500

    add more commit

--- WidgetCreate.js
+++ WidgetCreate.js
@@ -1,17 +1,17 @@
 
 
-// var link = "https://dinochat.glitch.me/visitor/visitorsignup";
-// var wHTML = "";
-//     wHTML += ('<iframe width="400" height="440" style="background: transparent;border: none;"'); 
-//     wHTML += ('src="'+ link +'"'); 
-//     wHTML += ('></iframe>'); 
-//     document.getElementById('ChatWidget').innerHTML = wHTML;
-
-
 var link = "https://dinochat.glitch.me/visitor/visitorsignup";
 var wHTML = "";
-    //wHTML += ('<div width="400" height="440" type="text/html" style="background: transparent;border: none;">'); 
-    wHTML += ('<embed width="400" height="440" src="'+ link +'">'); 
-    //wHTML += ('</div>'); 
+    wHTML += ('<iframe width="400" height="440" style="background: transparent;border: none;"'); 
+    wHTML += ('src="'+ link +'"'); 
+    wHTML += ('></iframe>'); 
     document.getElementById('ChatWidget').innerHTML = wHTML;
+
+
+// var link = "https://dinochat.glitch.me/visitor/visitorsignup";
+// var wHTML = "";
+//     //wHTML += ('<div width="400" height="440" type="text/html" style="background: transparent;border: none;">'); 
+//     wHTML += ('<embed width="400" height="440" src="'+ link +'">'); 
+//     //wHTML += ('</div>'); 
+//     document.getElementById('ChatWidget').innerHTML = wHTML;
 
