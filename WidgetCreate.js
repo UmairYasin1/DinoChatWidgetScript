19ca28e9aad9639cc68f9cdfc77df4e987b58747
Umair Yasin <umair_yasin@hotmail.com>
2020-11-03 00:55:19 +0500

    add more commit

--- WidgetCreate.js
+++ WidgetCreate.js
@@ -2,7 +2,14 @@
 
 var link = "https://dinochat.glitch.me/visitor/visitorsignup";
 var wHTML = "";
-    wHTML += ('<iframe width="400" height="440"'); 
+    wHTML += ('<iframe width="400" height="440" style="background: transparent;border: none;"'); 
     wHTML += ('src="'+ link +'"'); 
     wHTML += ('></iframe>'); 
     document.getElementById('ChatWidget').innerHTML = wHTML;
+
+// var link = "https://dinochat.glitch.me/visitor/visitorsignup";
+// // var wHTML = "";
+// //     wHTML += ('<iframe width="400" height="440"'); 
+// //     wHTML += ('src="'+ link +'"'); 
+// //     wHTML += ('></iframe>'); 
+//     document.getElementById('ChatWidget').innerHTML = link;
